90269037d16593f03fb71e056989146740d50528
Ahsan Ali <53314728+Iam-ahsan@users.noreply.github.com>
2022-08-15 08:32:05 -0400

    Update prepare_package.sh

--- lambda_package/prepare_package.sh
+++ lambda_package/prepare_package.sh
@@ -5,7 +5,7 @@ aws lambda create-function \
  --function-name deep_speech$i \
  --zip-file fileb://test.zip \
  --handler lambda_function.lambda_handler --runtime python3.7 \
---role arn:aws:iam::494717363231:role/service-role/deep_speech-role-eclmsdrd
+--role $Add_your_role_ARN
 }
 
 update_code(){
@@ -13,9 +13,9 @@ zip -g deepspeech_package.zip lambda_function.py
 aws s3 rm s3://deepspeech-package-new/deepspeech_package.zip
 aws s3 cp deepspeech_package.zip s3://deepspeech-package-new
 #echo "https://deepspeech-package.s3.amazonaws.com/deepspeech_package.zip"
-#aws lambda update-function-code --function-name deep_speech --s3-bucket deepspeech-package-new --s3-key deepspeech_package.zip
+#aws lambda update-function-code --function-name deep_speech --s3-bucket $NAME_OF_YOUR_BUCKET --s3-key deepspeech_package.zip
 }
-
+#We consider 32 buffers in this case at most
 for i in {0..31..1}
 do
 	echo $i
